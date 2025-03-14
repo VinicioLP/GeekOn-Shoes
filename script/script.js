ffe73208cc83d8cd50903f213da4a1b183b959a2
VinicioLPDev <viniciop757@gmail.com>
2025-03-14 19:27:48 -0300

    Adição de função para levar a outras páginas e melhor organização de arquivos

--- script/script.js
+++ script/script.js
@@ -1,6 +1,11 @@
 const navbar = document.querySelector(".navbar");
 const menuButton = document.querySelector(".menu-button");
+const products = document.getElementById("products");
 
 menuButton.addEventListener("click", () => {
   navbar.classList.toggle("show-menu");
+});
+
+products.addEventListener("click", function () {
+  window.location.href = "products.html";
 });
